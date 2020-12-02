74fdc16e7ab2a01db7067580c85fc207c64c654f
jaffarabbas <gamaportal8@gmail.com>
2020-12-02 18:27:31 +0500

    12-2-2020  basic inserted

--- app/src/main/java/com/listviewapp/MainActivity.java
+++ app/src/main/java/com/listviewapp/MainActivity.java
@@ -3,12 +3,42 @@ package com.listviewapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView mylist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //take id from list view
+        mylist = (ListView) findViewById(R.id.Listview);
+        //array list for view
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("apple");
+        list.add("Banna");
+        list.add("sdfsdf");
+        list.add("sdfsdf3");
+        list.add("sdfsdf2");
+        //adapter for set ArrayList in the ListView
+        ArrayAdapter<String> adapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
+        //set adapter to ArrayList
+        mylist.setAdapter(adapt);
+        //onclick view item detail
+        mylist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override//type cast text view to view Original string------------------------------->|
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {// \|/
+                String details = "Position : "+position+"\nId : "+id+"\nView : "+((TextView)view).getText().toString();
+                Toast.makeText(MainActivity.this,details,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
